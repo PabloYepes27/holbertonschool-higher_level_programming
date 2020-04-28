ad16617e7943462d031757d5738a92460410897f
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-04-28 03:27:31 +0000

    Task 10 - betty

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,13 +6,13 @@
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 
- int check_cycle(listint_t *list)
- {
+int check_cycle(listint_t *list)
+{
 	listint_t *aux1, *aux2;
 
-	*aux1 = list;
-	*aux2 = list;
-	while (aux1 != NULL & aux2 != NULL && aux1->next != NULL)
+	aux1 = list;
+	aux2 = list;
+	while (aux1 != NULL && aux2 != NULL && aux1->next != NULL)
 	{
 		aux1 = aux1->next->next;
 		aux2 = aux2->next;
@@ -22,4 +22,4 @@
 		}
 	}
 	return (0);
- }
+}
