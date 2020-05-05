e11a8495dae7279087279307403ff670b36111b1
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-05 23:10:06 +0000

    Task 5 - V2

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for i in range(0, len(my_string)):
-        if ord(my_string[i]) == 99 or ord(my_string[i]) == 67:
-            continue
-        else:
+        if ord(my_string[i]) != 99 and ord(my_string[i]) != 67:
             new_string = new_string + my_string[i]
     return new_string
