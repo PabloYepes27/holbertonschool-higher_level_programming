6711513d998204d4530309be72d08f70da89cffe
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-06 22:57:16 +0000

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,52 +1,61 @@
 #include "lists.h"
-listint_t *reverse_listint(listint_t **head);
+void reverse_listint(listint_t **head);
+size_t listint_len(const listint_t *h);
 
 /**
-  * is_palindrome - checks if a singly linked list is a palindrome
-  * @head: the first node
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+	* is_palindrome - checks if a singly linked list is a palindrome
+	* @head: the first node
+	* Return: 0 if it is not a palindrome, 1 if it is a palindrome
 **/
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head, *reversed;
-    int i = 1;
-
-	if (head == NULL)
+	listint_t *tmp = *head;
+	int i = 0, len = 0;
+	
+	if (head == NULL || (*head)->next == NULL)
 		return (1);
-    reversed = reverse_listint(head);
-	while (reversed)
+	len = listint_len(*head);
+	printf("\nlen before %d\n", len);
+	if (len % 2 != 0)
+		len = len / 2;
+	else
 	{
-        if (reversed->n == tmp->n)
-        {
-		    reversed = reversed->next;
-            tmp= tmp->next;
-            i = 0;
-        }
-        else
-        {
-            i = 1;
-            break;
-        }
-    }
-    if (i == 0)
-        return (1);
-	return (0);
+		len = len / 2;
+		len -= 1;
+	}
+	printf("len after %d\n", len);
+	while (i <= len)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	printf("list before reverse\n");
+	print_listint(*head);
+	reverse_listint(&tmp);
+	printf("list after reverse\n");
+	print_listint(*head);
+	while (tmp)
+	{
+
+		if (tmp->n != (*head)->n)
+			return (0);
+		*head = (*head)->next;
+		tmp = tmp->next;
+	}
+	return (1);
 }
 
-
 /**
-  * reverse_listint - function that reverses a listint_t linked list.
-  * @head: the first node
-  * Return: A pointer to the first node of the reversed list
+	* reverse_listint - function that reverses a listint_t linked list.
+	* @head: the first node
+	* Return: A pointer to the first node of the reversed list
 **/
 
-listint_t *reverse_listint(listint_t **head)
+void reverse_listint(listint_t **head)
 {
 	listint_t *first, *rest;
 
-	if (*head == NULL)
-		return (NULL);
 	first = NULL;
 	while (*head)
 	{
@@ -56,5 +65,19 @@ listint_t *reverse_listint(listint_t **head)
 		*head = rest;
 	}
 	*head = first;
-	return (*head);
+}
+
+/**
+  * listint_len - function that returns the number of elements in a linked list
+  * @h: the first node
+  * Return: The number of elements
+**/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+		h = h->next;/*move towards the next thing the actual node is pointing*/
+	return (i);
 }
