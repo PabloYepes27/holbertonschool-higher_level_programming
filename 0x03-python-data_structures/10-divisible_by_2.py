2cb5ccdef916acddea30a15cd3cb7722d11b7c1c
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-06 03:02:09 +0000

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return
     new_list = my_list[:]
     for num in my_list:
-        if num % 2 == 0:
+        if num % 2 > 0:
             new_list[num] = True
         else:
             new_list[num] = False
