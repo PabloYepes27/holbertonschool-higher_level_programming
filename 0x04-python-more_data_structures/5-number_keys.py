2a99ce0f3d1675b3cc5fb7cd6726a1be143dcaed
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-07 23:55:38 +0000

    Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """ function that returns the number of keys in a dictionary.
+
+    Arguments:
+        a_dictionary {dic} -- Dictionariy
+
+    Returns:
+        [int] -- the number of keys in a dictionary.
+    """
+    return len(a_dictionary.keys())
