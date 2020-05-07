d3599407422b4becc34c28a6259210d1bcde5138
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-07 20:38:31 +0000

    Task 0 - documentation

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """function that computes the square value of all integers of a matrix.
+
+    Keyword Arguments:
+        matrix {list} -- is a 2 dimensional array (default: {[]})
+
+    Returns:
+        [list] -- Returns a new matrix
+    """
     return [[elem ** 2 for elem in row] for row in matrix]
