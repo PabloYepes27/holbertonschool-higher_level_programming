cc262793834cb63b5157a59565ee1b8fd02b1853
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-07 23:19:26 +0000

    Task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """function that returns a set of common elements in two sets.
+
+    Arguments:
+        set_1 {set} -- Set 2
+        set_2 {set} -- Set 1
+
+    Returns:
+        [type] -- a set of common elements in two sets
+    """
+    return list(elem for elem in set_1 if elem in set_2)
