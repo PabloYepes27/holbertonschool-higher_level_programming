70c6ffb4627ae19e3361a34b1db5b9deb826e7b4
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-08 03:52:08 +0000

    Task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    """function that deletes keys with a specific value in a dictionary.
+
+    Arguments:
+        a_dictionary {dic} -- Dictionary
+        value {str} -- value to check in dictionary
+
+    Returns:
+        [type] -- The directory modified or the same if the value doesn't exist
+    """
+    tmp_dictionary = a_dictionary.copy()
+    for key, val in tmp_dictionary.items():
+        if val == value:
+            a_dictionary.pop(key)
+    return tmp_dictionary
