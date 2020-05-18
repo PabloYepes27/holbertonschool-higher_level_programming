ebb4c2f9286d66b70bf9b35f1077a4817f89be53
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-18 21:38:30 +0000

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = a/b
+    except ZeroDivisionError:
+        c = None
+    except UnboundLocalError:
+        c = None
+    finally:
+        print("Inside Result: {}".format(c))
+    return c
