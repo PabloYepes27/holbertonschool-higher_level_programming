cc2dea8531a34b3d678246ff3dba6c8a41b8254c
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-19 01:05:02 +0000

    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 def safe_function(fct, *args):
     import sys
     try:
-        var = fct(args[0], args[1])
+        var = fct(*args)
     except (ValueError, TypeError, ZeroDivisionError, IndexError) as err:
         print("Exception:", err, file=sys.stderr)
         return None
