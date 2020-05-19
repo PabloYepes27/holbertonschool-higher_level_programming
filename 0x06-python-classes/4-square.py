8f02ad5d5409c5f912577576c1125fde02becdd3
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-19 22:02:40 +0000

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+" A class Square that defines a square "
+
+
+class Square:
+    """
+    Square class
+
+    Attributes:
+        size (int) : Private instance attribute
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    """
+    Public instance method that returns the current square area
+    """
+    def area(self):
+        return self.__size ** 2
