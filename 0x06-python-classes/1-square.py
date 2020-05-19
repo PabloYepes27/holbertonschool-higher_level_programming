40cf19914852cfa678e07a5fd01d51296be8f405
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-19 20:21:32 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class Square that defines a square
+
+    Attributes:
+        size (no type/value verification) : Private instance attribute
+    """
+    def __init__(self, size):
+        self.__size = size
