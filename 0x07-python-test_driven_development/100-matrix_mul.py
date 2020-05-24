b7325c6546c28b128dc986e8fa09e72b67e67acd
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-05-24 16:17:02 +0000

    Test cases

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -51,10 +51,10 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_b can't be empty")
 
     if len(set([len(row) for row in m_a])) is not 1:
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
 
     if len(set([len(row) for row in m_b])) is not 1:
-        raise TypeError("each row of m_b must should be of the same size")
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
