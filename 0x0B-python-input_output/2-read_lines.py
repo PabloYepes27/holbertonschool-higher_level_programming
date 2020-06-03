085fb4093debecd455795446a9d96ff184ef74c8
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-06-03 20:53:10 +0000

    Task 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""[]"""
+"""[Write a function that reads n lines of a text file
+ (UTF8) and prints it to stdout:]"""
 
 
 def read_lines(filename="", nb_lines=0):
-    """[summary]
+    """[read() prit all the text, readline() print each line until
+     '\n' is found]
 
     Args:
-        filename (str, optional): [description]. Defaults to "".
-        nb_lines (int, optional): [description]. Defaults to 0.
+        filename (str, optional): [name of the file to
+         open and read]. Defaults to "".
+        nb_lines (int, optional): [name of the lines to
+         print]. Defaults to 0.
     """
     with open(filename, encoding="UTF8") as MyFile:
         if nb_lines > 0:
