04fdad6da9a2adc9e5e7f3a494d0cdbba2160751
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-06-04 02:41:17 +0000

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student"""
+class Student:
+    """
+        Student to JSON
+    """
+    def __init__(self, first_name, last_name, age):
+        """constructor
+
+        Args:
+            first_name ([type]): [description]
+            last_name ([type]): [description]
+            age ([type]): [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Public method def to_json(self): that
+         retrieves a dictionary representation of
+          a Student instance
+
+        Returns:
+            [dict]: [dictionary representation of
+             a Student instance]
+        """
+        return self.__dict__
