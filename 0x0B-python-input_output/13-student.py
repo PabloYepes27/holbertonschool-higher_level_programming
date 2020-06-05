c4b5055c7972f860b1ab658864a7cd5e97140739
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-06-05 00:09:02 +0000

    Task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student
+ by: (based on 12-student.py)"""
+
+
+class Student:
+    """
+        Student to JSON
+    """
+    def __init__(self, first_name, last_name, age):
+        """constructor
+
+        Args:
+            first_name ([type]): [description]
+            last_name ([type]): [description]
+            age ([type]): [description]
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method def to_json(self, attrs=None):
+         that retrieves a dictionary representation of
+          a Student instance (same as 10-class_to_json.py):
+
+        Returns:
+            [dict]: [dictionary representation of
+             a Student instance]
+        """
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for elem in attrs:
+            if elem in self.__dict__:
+                dic[elem] = self.__dict__[elem]
+        return dic
+
+    def reload_from_json(self, json):
+        """another way is < self.__dict__[key] = dic[key] >
+
+        Args:
+            json ([dict]): [description]
+        """
+        dic = dict(json)
+        for key in dic:
+            self.__dict__[key] = dic[key]
