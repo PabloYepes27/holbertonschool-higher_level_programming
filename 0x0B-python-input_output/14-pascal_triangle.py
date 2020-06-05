0c5a8e8417f468d1e1c642fac017def75cba3396
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-06-05 01:12:47 +0000

    Task 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""  """
+""" Create a function def pascal_triangle(n): that returns a list
+ of lists of integers representing the Pascal’s triangle of n: """
 
 
 def pascal_triangle(n):
-    """[summary]
+    """Returns an empty list if n <= 0
+You can assume n will be always an integer
 
     Args:
         n ([type]): [description]
