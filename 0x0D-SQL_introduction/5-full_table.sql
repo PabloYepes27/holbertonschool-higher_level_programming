0343e68ecf09723009a9010f683d770183c8dcdc
PabloYepes27 <juanpabloyepest@gmail.com>
2020-07-13 23:13:39 +0000

    Task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- SHOW CREATE DATABASE databaseName -> Show the CREATE DATABASE statement
+SHOW CREATE TABLE first_table
