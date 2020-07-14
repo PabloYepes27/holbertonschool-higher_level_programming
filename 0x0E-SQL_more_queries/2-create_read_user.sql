f93aa809ef73b1f5aa8efffd5d0373261e35dbd6
PabloYepes27 <juanpabloyepest@gmail.com>
2020-07-14 21:55:13 +0000

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_2_pwd');
+SET PASSWORD FOR 'user_0d_2'@'localhost' = PASSWORD('user_0d_2_pwd');
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
