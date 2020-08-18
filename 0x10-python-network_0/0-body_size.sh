1f8b5009aac4b21f42b97571bcf9ede4bf1cbe27
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-08-18 16:49:20 -0500

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-# Write a Bash script that takes in a URL, sends a
-# request to that URL, and displays the size of the body
-# of the response
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep Content-Length | awk '{print $2}'
