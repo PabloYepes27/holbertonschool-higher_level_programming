c1556f821082b92d43e984492cdae9414b70e64c
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-08-18 17:53:36 -0500

    Task 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -X GET -L "$1/apply?X-HolbertonSchool-User-Id=98"
+curl -s -X GET -L "$1/apply?X-HolbertonSchool-User-Id:98"
