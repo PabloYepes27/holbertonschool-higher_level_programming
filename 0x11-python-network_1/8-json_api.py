dc4bdc15a6dbc1f8a3f1419972841429befeb24a
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-08-24 20:24:35 -0500

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a
+request to the URL and displays the body of the response.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        values = {'q': sys.argv[1]}
+    else:
+        values = {'q': ""}
+    req = requests.post(url, data=values)
+    if (len(req.json()) is not 0 and req.json()):
+        try:
+            req.json()
+            print('[{}] {}'.format(req.json()['id'], req.json()['name']))
+        except:
+            print("Not a valid JSON")
+    else:
+        print("No result")
