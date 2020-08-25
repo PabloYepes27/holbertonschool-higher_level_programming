f0835886e39bfa8274248b8e2e9fbb3c454470f2
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-08-24 22:40:22 -0500

    Task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,9 +11,9 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     req = requests.get(url)
     for i in range(10):
-        commit = req.json()[i].get('sha')
+        sha = req.json()[i].get('sha')
         name = req.json()[i].get('commit').get('author').get('name')
-        print('{}: {}'.format(commit, name))
+        print('{}: {}'.format(sha, name))
