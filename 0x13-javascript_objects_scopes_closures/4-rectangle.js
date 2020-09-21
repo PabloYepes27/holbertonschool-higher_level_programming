22bee49cf785cab2165449b9fc9d0813edd31775
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-09-21 12:19:08 -0500

    Task 4 checks red

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 // The constructor is used to stablished the object properties or to call the methods to prepare the object for use
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.height = h;
       this.width = w;
     }
-  }
-
-  // Getter
-  get printRec () {
-    return this.print();
   }
 
   // method that prints a rectangle
@@ -20,11 +15,6 @@ class Rectangle {
     }
   }
 
-  // Getter
-  get rotateRec () {
-    return this.rotate();
-  }
-
   // method that prints a rectangle
   rotate () {
     const a = this.width;
@@ -32,15 +22,9 @@ class Rectangle {
     this.height = a;
   }
 
-  // Getter
-  get doubleRec () {
-    return this.double();
-  }
-
   // method that prints a rectangle
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-}
-module.exports = Rectangle;
+};
