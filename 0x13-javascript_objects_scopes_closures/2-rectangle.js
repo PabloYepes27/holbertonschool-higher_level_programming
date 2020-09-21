f849f32399ea8cc12d9721532c2ec641a10228e7
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-09-21 12:11:49 -0500

    Test check 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// The constructor is used to stablished the object properties or to call the methods to prepare the object for use
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
