01ab9ee2450ed2972da648b008a196fb59f0f2e7
Juan pablo Yepes <juanpabloyepest@gmail.com>
2020-09-21 12:25:05 -0500

    Task 6 this instead of super

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 // Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
 const square = require('./5-square');
 
-class Square extends square {
+module.exports = class Square extends square {
   // method that prints a rectangle usign 'C' char
   charPrint (c) {
     if (c === undefined) {
@@ -13,5 +13,4 @@ class Square extends square {
       }
     }
   }
-}
-module.exports = Square;
+};
